9d32cdf90969f527424b95fb67683f7152a21727
luiq54 <dev.darshanphaldesai@gmail.com>
2023-06-09 00:43:33 +0530

    add Essence capability

---
+++ src/main/java/com/luiq54/ArsCruor/common/capability/CapabilityRegistry.java
@@ -0,0 +1,55 @@
+package com.luiq54.ArsCruor.common.capability;
+
+import com.luiq54.ArsCruor.ArsCruor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class CapabilityRegistry {
+    public static final Capability<IEssenceCap> ESSENCE_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
+    });
+
+    public static LazyOptional<IEssenceCap> getEssence(final LivingEntity entity) {
+        if (entity == null)
+            return LazyOptional.empty();
+        return entity.getCapability(ESSENCE_CAPABILITY);
+    }
+
+    @Mod.EventBusSubscriber(modid = ArsCruor.MODID)
+    public static class EventHandler {
+
+        @SubscribeEvent
+        public static void attachCapabilities(final AttachCapabilitiesEvent<Entity> event) {
+            if (event.getObject() instanceof Player) {
+                EssenceCapAttacher.attach(event);
+            }
+        }
+
+        @SubscribeEvent
+        public static void registerCapabilities(final RegisterCapabilitiesEvent event) {
+            event.register(IEssenceCap.class);
+        }
+
+        @SubscribeEvent
+        public static void playerClone(PlayerEvent.Clone event) {
+            Player oldPlayer = event.getOriginal();
+            oldPlayer.revive();
+            getEssence(oldPlayer).ifPresent(oldMaxEssence -> getEssence(event.getEntity()).ifPresent(newMaxEssence -> {
+                newMaxEssence.setMaxEssence(oldMaxEssence.getMaxEssence());
+                newMaxEssence.setEssence(oldMaxEssence.getCurrentEssence());
+            }));
+
+            event.getOriginal().invalidateCaps();
+        }
+
+    }
+}
