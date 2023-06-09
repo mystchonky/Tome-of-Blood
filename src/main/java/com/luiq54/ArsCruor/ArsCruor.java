24609e55c95cd77040f770c33b28a92c14f3e65b
luiq54 <dev.darshanphaldesai@gmail.com>
2023-06-09 16:01:31 +0530

    Add Essence events and networking

--- src/main/java/com/luiq54/ArsCruor/ArsCruor.java
+++ src/main/java/com/luiq54/ArsCruor/ArsCruor.java
@@ -1,11 +1,16 @@
 package com.luiq54.ArsCruor;
 
+import com.hollingsworth.arsnouveau.setup.ClientProxy;
+import com.hollingsworth.arsnouveau.setup.IProxy;
+import com.hollingsworth.arsnouveau.setup.ServerProxy;
+import com.luiq54.ArsCruor.common.network.Networking;
 import com.luiq54.ArsCruor.registry.ModRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -17,6 +22,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(ArsCruor.MODID)
 public class ArsCruor {
     public static final String MODID = "arscruor";
+    public static IProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new); //TODO: Change to our own
 
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -35,6 +41,7 @@ public class ArsCruor {
 
     private void setup(final FMLCommonSetupEvent event) {
         ArsNouveauRegistry.registerSounds();
+        Networking.registerMessages();
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
