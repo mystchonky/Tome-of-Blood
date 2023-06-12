12885d45c7499b5b2a142e4f677d897ea5ca1d6a
luiq54 <dev.darshanphaldesai@gmail.com>
2023-06-13 00:40:29 +0530

    add new texture for essence bar

--- src/main/java/com/luiq54/arsoscura/client/gui/GuiEssenceHUD.java
+++ src/main/java/com/luiq54/arsoscura/client/gui/GuiEssenceHUD.java
@@ -1,7 +1,6 @@
 package com.luiq54.arsoscura.client.gui;
 
 import com.luiq54.arsoscura.ArsOscura;
-import com.luiq54.arsoscura.client.ClientInfo;
 import com.luiq54.arsoscura.common.capability.CapabilityRegistry;
 import com.luiq54.arsoscura.common.capability.IEssenceCap;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -26,23 +25,22 @@ public class GuiEssenceHUD extends GuiComponent {
         if (essenceCap == null) // FIXME: why does this exist, remove if unnecessary
             return;
 
-        int offsetLeft = 10;
+        int xOffset = 10;
 //        int offsetLeft = 10 + Config.MANABAR_X_OFFSET.get();
-        int manaLength = 96;
+        int manaLength = 98;
         manaLength = (int) ((manaLength) * ((essenceCap.getCurrentEssence()) / ((double) essenceCap.getMaxEssence() - 0.0)));
 
-        int yOffset = minecraft.getWindow().getGuiScaledHeight() - 28;
+        int yOffset = minecraft.getWindow().getGuiScaledHeight() - 28 - 18;
 //        int yOffset = minecraft.getWindow().getGuiScaledHeight() - 5 + Config.MANABAR_Y_OFFSET.get();
 
-        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/manabar_gui_border.png"));
-        blit(ms, offsetLeft, yOffset - 18, 0, 0, 108, 18, 256, 256);
-        int manaOffset = (int) (((ClientInfo.ticksInGame + pt) / 3 % (33))) * 6;
+        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/essencebar_gui.png"));
+        blit(ms, xOffset, yOffset, 4, 4, 102, 18, 256, 256);
 
         // 96
-        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/manabar_gui_mana.png"));
-        blit(ms, offsetLeft + 9, yOffset - 9, 0, manaOffset, manaLength, 6, 256, 256);
+        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/essencebar_gui.png"));
+        blit(ms, xOffset + 2, yOffset + 6, 6, 29, manaLength, 8, 256, 256);
 
-        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/manabar_gui_border.png"));
-        blit(ms, offsetLeft, yOffset - 17, 0, 18, 108, 20, 256, 256);
+        RenderSystem.setShaderTexture(0, new ResourceLocation(ArsOscura.MODID, "textures/gui/essencebar_gui.png"));
+        blit(ms, xOffset, yOffset, 4, 40, 102, 18, 256, 256);
     }
 }
