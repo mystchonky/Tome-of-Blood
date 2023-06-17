cdf03705c978627ea988daff795b59e3ca04eda1
luiq54 <dev.darshanphaldesai@gmail.com>
2023-06-17 21:10:15 +0530

    reimplement sentient harm

--- src/main/java/com/luiq54/arsoscura/ArsNouveauRegistry.java
+++ src/main/java/com/luiq54/arsoscura/ArsNouveauRegistry.java
@@ -2,15 +2,19 @@ package com.luiq54.arsoscura;
 
 import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
 import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
+import com.hollingsworth.arsnouveau.api.spell.SpellSchool;
+import com.luiq54.arsoscura.common.glyphs.EffectSentientHarm;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArsNouveauRegistry {
 
+    public static SpellSchool BLOODMAGIC = new SpellSchool("bloodmagic");
     public static List<AbstractSpellPart> registeredSpells = new ArrayList<>(); //this will come handy for datagen
 
     public static void registerGlyphs() {
+        register(EffectSentientHarm.INSTANCE);
 //        register(EffectLifeSuck.INSTANCE);
 //        register(EffectSigilGenerate.INSTANCE);
 //        register(MethodSigil.INSTANCE);
