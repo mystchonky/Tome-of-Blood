872946c85bfa0d51ab097444a730365426045e81
mystchonky <dev.darshanphaldesai@gmail.com>
2023-07-27 21:01:45 +0530

    spirits can now pick up items

--- src/main/java/com/mystchonky/arsoscura/common/integration/occultism/mob_jar/SpiritBehaviour.java
+++ src/main/java/com/mystchonky/arsoscura/common/integration/occultism/mob_jar/SpiritBehaviour.java
@@ -7,13 +7,17 @@ import com.klikli_dev.occultism.common.entity.spirit.SpiritEntity;
 import com.klikli_dev.occultism.exceptions.ItemHandlerMissingException;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
+
+import java.util.List;
 
 public class SpiritBehaviour<T extends SpiritEntity> extends JarBehavior<T> {
 
@@ -45,6 +49,21 @@ public class SpiritBehaviour<T extends SpiritEntity> extends JarBehavior<T> {
         SpiritEntity spirit = entityFromJar(tile);
         if (!spirit.isInitialized())
             spirit.init();
+
+        if (tile.getLevel().getRandom().nextInt(20) == 0) {
+            List<ItemEntity> itemEntities = spirit.level.getEntitiesOfClass(ItemEntity.class, new AABB(tile.getBlockPos()).inflate(3), ItemEntity::isAlive);
+            if (!itemEntities.isEmpty()) {
+                ItemEntity itemEntity = itemEntities.stream().filter(item -> OccultismAPI.get().canPickupItem(spirit, item).orElse(false)).findFirst().orElse(null);
+                if (itemEntity != null) {
+                    ItemStack duplicate = itemEntity.getItem().copy();
+                    ItemStackHandler handler = spirit.itemStackHandler.orElseThrow(ItemHandlerMissingException::new);
+                    if (ItemHandlerHelper.insertItemStacked(handler, duplicate, true).getCount() < duplicate.getCount()) {
+                        ItemStack remaining = ItemHandlerHelper.insertItemStacked(handler, duplicate, false);
+                        itemEntity.getItem().setCount(remaining.getCount());
+                    }
+                }
+            }
+        }
         spirit.getJob().ifPresent(spiritJob -> {
             spiritJob.update();
             tile.updateBlock();
