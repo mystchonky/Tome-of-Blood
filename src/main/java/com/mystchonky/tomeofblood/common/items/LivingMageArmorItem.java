c5cababdbb76d003e8f781947f6a6cac782f90a2
mystchonky <dev.darshanphaldesai@gmail.com>
2023-10-13 16:39:55 +0530

    fix armor error and start patchouli guide

--- src/main/java/com/mystchonky/tomeofblood/common/items/LivingMageArmorItem.java
+++ src/main/java/com/mystchonky/tomeofblood/common/items/LivingMageArmorItem.java
@@ -2,13 +2,17 @@ package com.mystchonky.tomeofblood.common.items;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import com.hollingsworth.arsnouveau.client.renderer.tile.GenericModel;
 import com.hollingsworth.arsnouveau.common.armor.AnimatedMagicArmor;
+import com.mystchonky.tomeofblood.TomeOfBlood;
 import com.mystchonky.tomeofblood.client.renderer.ToBGenericModel;
 import com.mystchonky.tomeofblood.client.renderer.item.LivingMageArmorRenderer;
 import com.mystchonky.tomeofblood.common.registry.ItemRegistry;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -21,6 +25,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import wayoftime.bloodmagic.common.item.ArmorMaterialLiving;
 import wayoftime.bloodmagic.common.item.ExpandedArmor;
@@ -101,6 +106,12 @@ public class LivingMageArmorItem extends AnimatedMagicArmor implements ILivingCo
     public void appendHoverText(ItemStack stack, Level world, List<Component> tooltip, TooltipFlag flag) {
         super.appendHoverText(stack, world, tooltip, flag);
         ILivingContainer.appendLivingTooltip(stack, getLivingStats(stack), tooltip, true);
+    }
+
+    @Override
+    public @Nullable String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+        GenericModel<AnimatedMagicArmor> genericModel = (GenericModel<AnimatedMagicArmor>) model;
+        return new ResourceLocation(TomeOfBlood.MODID, "textures/" + genericModel.textPathRoot + "/" + genericModel.name + "_" + this.getColor(stack) + ".png").toString();
     }
 
     @Override
