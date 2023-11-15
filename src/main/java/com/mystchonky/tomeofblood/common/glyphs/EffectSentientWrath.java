8db5ff3f40e3186763ab1b0738309ba7e96075a2
mystchonky <dev.darshanphaldesai@gmail.com>
2023-11-15 23:11:40 +0530

    add special behaviour for SentientWrath

--- src/main/java/com/mystchonky/tomeofblood/common/glyphs/EffectSentientWrath.java
+++ src/main/java/com/mystchonky/tomeofblood/common/glyphs/EffectSentientWrath.java
@@ -15,6 +15,7 @@ import com.hollingsworth.arsnouveau.common.spell.augment.AugmentDampen;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentDurationDown;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentExtendTime;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentFortune;
+import com.hollingsworth.arsnouveau.setup.registry.ModPotions;
 import com.mystchonky.tomeofblood.TomeOfBlood;
 import com.mystchonky.tomeofblood.client.registry.ParticleRegistry;
 import com.mystchonky.tomeofblood.common.registry.IntegrationRegistry;
@@ -23,6 +24,7 @@ import com.mystchonky.tomeofblood.common.util.DemonWillUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -32,8 +34,11 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.jetbrains.annotations.NotNull;
 import wayoftime.bloodmagic.api.compat.EnumDemonWillType;
+import wayoftime.bloodmagic.common.item.soul.ItemSentientSword;
 import wayoftime.bloodmagic.will.PlayerDemonWillHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -51,34 +56,49 @@ public class EffectSentientWrath extends AbstractEffect implements IDamageEffect
             return;
         if (!(shooter instanceof Player player))
             return;
+
         EnumDemonWillType type = PlayerDemonWillHandler.getLargestWillType(player);
         int souls = (int) PlayerDemonWillHandler.getTotalDemonWill(type, player);
         int bracket = DemonWillUtil.getBracket(type, souls);
         Vec3 vec = safelyGetHitPos(rayTraceResult);
-        float damage = (float) (DAMAGE.get() + AMP_VALUE.get() * spellStats.getAmpMultiplier());
+        float baseDamage = (float) (DAMAGE.get() + AMP_VALUE.get() * spellStats.getAmpMultiplier());
         double range = 3 + spellStats.getAoeMultiplier();
         int duration = (int) (POTION_TIME.get() + EXTEND_TIME.get() * spellStats.getDurationMultiplier());
 
+        List<LivingEntity> targets = new ArrayList<>();
+
         if (!target.hasEffect(MobEffectRegistry.CURSED.get())) {
             target.addEffect(new MobEffectInstance(MobEffectRegistry.CURSED.get(), 20 * duration, 1));
-            attemptDamage(level, shooter, spellStats, spellContext, resolver, target, buildDamageSource(level, shooter), damage);
+            attemptDamage(level, shooter, spellStats, spellContext, resolver, target, buildDamageSource(level, shooter), baseDamage);
+            targets.add(target);
         } else {
-            heavyAttack(vec, level, shooter, target, spellStats, spellContext, resolver, duration, damage);
+            // heavy attack all targets
+            float heavyDamage = baseDamage + 2.0f + DemonWillUtil.getExtraDamage(type, bracket);
+            heavyAttack(vec, level, shooter, target, spellStats, spellContext, resolver, duration, heavyDamage);
+            targets.add(target);
+
             for (LivingEntity mob : world.getEntitiesOfClass(LivingEntity.class, new AABB(target.position().add(range, range, range), target.position().subtract(range, range, range)))) {
                 if (mob.equals(target) || mob.equals(shooter))
                     continue;
                 vec = mob.position();
-                heavyAttack(vec, level, shooter, mob, spellStats, spellContext, resolver, duration, damage);
+                heavyAttack(vec, level, shooter, mob, spellStats, spellContext, resolver, duration, heavyDamage);
+                targets.add(mob);
             }
         }
 
+        // Consume will
+        double willCost = ItemSentientSword.soulDrainPerSwing[bracket] * targets.size() * (targets.size() > 1 ? 0.75 : 1);
+        PlayerDemonWillHandler.consumeDemonWill(type, player, willCost);
+
         //trigger on hit
+        targets.stream().filter(it -> !it.isAlive()).findAny().ifPresent(it -> applyEffectOnKill(player, type, duration));
+
 
     }
 
-    public void heavyAttack(Vec3 vec, ServerLevel level, LivingEntity shooter, LivingEntity target, SpellStats stats, SpellContext context, SpellResolver resolver, int duration, float baseDamage) {
+    public void heavyAttack(Vec3 vec, ServerLevel level, LivingEntity shooter, LivingEntity target, SpellStats stats, SpellContext context, SpellResolver resolver, int duration, float damage) {
         // calculate heavy damage
-        if (attemptDamage(level, shooter, stats, context, resolver, target, buildDamageSource(level, shooter), baseDamage)) {
+        if (attemptDamage(level, shooter, stats, context, resolver, target, buildDamageSource(level, shooter), damage)) {
             level.sendParticles(ParticleRegistry.WRATH_SLASH.get(), vec.x, vec.y + 0.5, vec.z, 1,
                     ParticleUtil.inRange(-0.1, 0.1), ParticleUtil.inRange(-0.1, 0.1), ParticleUtil.inRange(-0.1, 0.1), 0.3);
             target.addEffect(new MobEffectInstance(MobEffectRegistry.CURSED.get(), 20 * duration));
@@ -86,12 +106,30 @@ public class EffectSentientWrath extends AbstractEffect implements IDamageEffect
 
     }
 
+    public void applyEffectOnKill(Player player, EnumDemonWillType type, int duration) {
+        if (type == EnumDemonWillType.DEFAULT)
+            return;
+        MobEffectInstance instance = switch (type) {
+            case CORROSIVE -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, duration * 20);
+            case VENGEFUL -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, duration * 20);
+            case STEADFAST -> new MobEffectInstance(MobEffects.ABSORPTION, duration * 20, 127);
+            case DESTRUCTIVE -> new MobEffectInstance(ModPotions.SPELL_DAMAGE_EFFECT.get(), duration * 20);
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        };
+
+        player.addEffect(instance);
+        if (type == EnumDemonWillType.STEADFAST) {
+            float extra = player.getAbsorptionAmount();
+            player.setAbsorptionAmount(Math.min(extra + player.getMaxHealth() * 0.2f, 10));
+        }
+    }
+
     @Override
     public void buildConfig(ForgeConfigSpec.Builder builder) {
         super.buildConfig(builder);
-        addDamageConfig(builder, 6.0);
-        addAmpConfig(builder, 2.5);
-        addPotionConfig(builder, 10);
+        addDamageConfig(builder, 3.0);
+        addAmpConfig(builder, 2.0);
+        addPotionConfig(builder, 20);
         addExtendTimeConfig(builder, 5);
     }
 
